14ebaf9f73e27bc57d94309e9eb8616a2fa08d40
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-08-31 22:35:04 -0700

    quiz task by sir

---
+++ java.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "What is the capital of Japan?",
+        answers: ["(0) 0Berlin", "(1) Madrid", "(2) Paris", "(3) Tokyo"],
+        correct: 3 
+    },
+    {
+        question: "what is 3+2 ",
+        answers: ["3", "4", "5"],
+        correct: 1
+    },
+    {
+        question: "who is the current prime minister of pakistan",
+        answers: ["Imran Khan", "Asif Ali Zardari", "Mian Muhammad Shahbaz Sharif"],
+        correct: 2
+    },
+    {
+        question: "Laimon Contain WHich Type of Acid",
+        answers: ["Acitic Acid", "Luminous Acid", "Non Of These"],
+        correct: 0
+    },
+    
+];
+
+var currentQuestionIndex = 0;
+var score = 0;
+
